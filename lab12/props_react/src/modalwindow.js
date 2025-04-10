48ab2f904f7608a7dc610fe0cd7ec90243041a44
Dante Vanderpool <vanderpoolikes@gmail.com>
2025-04-10 20:07:26 +0000

    complete lab 12 up to close modal window

--- lab12/props_react/src/modalwindow.js
+++ lab12/props_react/src/modalwindow.js
@@ -18,7 +18,7 @@ const Modalwindow = function(props){
                         <p className="closemdal" onClick={closemodalwindow}>&#x58;</p>
                     </header>
                     <main className="modalbody">
-                        <input className="commentarea" type="text" placeholder="Type ypur comments"/>
+                        <input className="commentarea" type="text" placeholder="Type your comments"/>
                         <p className="description_comment">Max 200 Characters</p>
                         <button className="btnpostfeedback">Post feedback</button>
                     </main>
