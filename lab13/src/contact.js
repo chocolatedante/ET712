7f5c35f5648be4b68095a74ffa10ca6b430ee69a
Dante Vanderpool <vanderpoolikes@gmail.com>
2025-04-22 19:51:21 +0000

    complete React Routing

---
+++ lab13/src/contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = function(){
+    return(
+        <>
+            <header ClassName ="title"><h1>Contact</h1></header>
+        </>
+    )
+}
+
+export default Contact
---
+++ lab13/src/navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "./App.css"
+
+
+const Navbars= function(){
+    return(
+        <>
+            <header ClassName="title"><h1>Navigation bars</h1></header>
+            <nav ClassNane="navheader">
+                <section ClassName="iconlink">
+                    <a href="https://www.qcc.cuny.edu" target="_blank">QCC</a>
+                </section>
+                <section ClassName="navlinks">
+                    <button ClassName ="tablink"><Link to ="/">Home</Link></button>
+                    <button ClassName ="tablink"><Link to = "/about">About Us</Link></button>
+                    <button ClassName ="tablink"><Link to = "/contact">Contact Us</Link></button>
+                </section>
+            </nav>
+        </>
+    )
+}
+
+export default Navbars
