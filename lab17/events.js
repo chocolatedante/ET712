7b24c73532c1c2b3da72b0429c952455883a1963
Dante Vanderpool <vanderpoolikes@gmail.com>
2025-05-06 19:57:30 +0000

    complete lab

---
+++ lab17/events.js
@@ -0,0 +1,35 @@
+const events = require('events')
+const util = require('util')
+
+const teams = function(name){
+    this.name = name
+}
+// eventEmitter will inheret any teams construct
+util.inherits(teams, events.EventEmitter)
+
+const Barcelona = new teams('Barcelona')
+const Milan = new teams('Milan')
+
+// save each constructor in an array
+const teamsArray = [Barcelona, Milan]
+
+// print each team using forEach loop
+teamsArray.forEach((t)=>{
+    t.on('nation', function(n){
+        console.log(`${t.name} is ${n} football club!`)
+    })
+})
+
+Milan.emit('nation', 'Italian')
+
+
+/*
+
+const eventEmitter = new event.EventEmitter
+
+eventEmitter.on('test', function(a){
+    console.log(a)
+})
+
+eventEmitter.emit('test','EVENTS IN NODEJS')
+*/
